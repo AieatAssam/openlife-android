e1fc574717afccefb7052679ad8ec57ad09b2b33
OpenLife <mistreated@gmail.com>
2026-09-19 20:47:58 +0000

    P0-05: Fix version test formatting
    Tests rerun: detekt, lint, app and vault tests, and debug assemble.
    Docs updated: none.

--- app/src/test/java/org/openlife/app/version/VersionCodeTest.kt
+++ app/src/test/java/org/openlife/app/version/VersionCodeTest.kt
@@ -1,9 +1,9 @@
 package org.openlife.app.version
 
-import java.nio.file.Files
-import java.nio.file.Path
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.nio.file.Files
+import java.nio.file.Path
 
 class VersionCodeTest {
 
